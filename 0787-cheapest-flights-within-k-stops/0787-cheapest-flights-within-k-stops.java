1d0ecbb6549d27495dc144a6a54404edb5bd74dd
gudeshgupta <gudeshgupta123@gmail.com>
2026-09-04 19:41:05 +0530

    Time: 4 ms (99.71%), Space: 46.9 MB (11.01%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] res= new int[n];
+        int inf=100000000;
+        Arrays.fill(res,inf);
+        res[src]=0;
+       
+
+        for(int i=0;i<=k;i++){
+            int[] temp = res.clone();
+
+            for(int j=0;j<flights.length;j++){
+                int s=flights[j][0];
+                int d=flights[j][1];
+                int wt=flights[j][2];
+
+                if(res[s]!=inf && temp[d]>res[s]+wt){
+                    temp[d]=res[s]+wt;
+                }
+            }
+            res=temp;
+        }
+        if(res[dst]==inf)
+        return -1;
+
+        return res[dst];
+    }
+}
